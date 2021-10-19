d1ceb1d97e130755a2532cd1265eb8e31d9c73a4
ERICKGZ87 <erickgz87@gmail.com>
2021-10-19 23:03:33 +0000

    react

--- src/Layout.js
+++ src/Layout.js
@@ -7,18 +7,17 @@ import {Footer} from "./Componentes/Footer";
 export const Layout =() => {
 
     return (
-        <div className="container">
+        <div className="container-fluid">
             <div className="row">
 
-                <div className="col-sm-2" />
+                
 
-                <div className="col-sm-8">
+                <div className="col-sm-12">
 
                   <Navbarr/>
 
                 </div>
 
-                <div className="col-sm-2" />
 
             </div>
 
